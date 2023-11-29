1fc20f73d18c2b5a1e7dc60db3feda5dd78d216f
Edmond <edmond.nyamosi@strathmore.edu>
2023-11-29 23:42:54 +0300

    Minor change

---
+++ js/classes/Enemy.js
@@ -0,0 +1,78 @@
+class Enemy extends Sprite {
+  constructor({ position = { x: 0, y: 0 } }) {
+    super({
+      position,
+      imageSrc: 'img/orc.png',
+      frames: {
+        max: 7
+      }
+    })
+    this.position = position
+    this.width = 100
+    this.height = 100
+    this.waypointIndex = 0
+    this.center = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y + this.height / 2
+    }
+    this.radius = 50
+    this.health = 130
+    this.velocity = {
+      x: 0,
+      y: 0
+    }
+    // Play marching sound when an enemy is created
+    // this.marchingSound = new Audio('sounds/marching-loop-32908.mp3');
+    // this.marchingSound.loop = true;
+    // this.marchingSound.play();
+  }
+
+  draw() {
+    super.draw()
+
+    // health bar
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y - 15, this.width, 10)
+
+    c.fillStyle = 'green'
+    c.fillRect(
+      this.position.x,
+      this.position.y - 15,
+      (this.width * this.health) / 130,
+      10
+    )
+  }
+
+  update() {
+    this.draw()
+    super.update()
+
+    const waypoint = waypoints[this.waypointIndex]
+    const yDistance = waypoint.y - this.center.y
+    const xDistance = waypoint.x - this.center.x
+    const angle = Math.atan2(yDistance, xDistance)
+
+    const speed = 3.5
+
+    this.velocity.x = Math.cos(angle) * speed
+    this.velocity.y = Math.sin(angle) * speed
+
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
+
+    this.center = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y + this.height / 2
+    }
+
+    if (
+      Math.abs(Math.round(this.center.x) - Math.round(waypoint.x)) <
+        Math.abs(this.velocity.x) &&
+      Math.abs(Math.round(this.center.y) - Math.round(waypoint.y)) <
+        Math.abs(this.velocity.y) &&
+      this.waypointIndex < waypoints.length - 1
+    ) {
+      this.waypointIndex++
+    }
+  }
+}
